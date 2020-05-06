74dd427a6f2f098c7b6c9365ed61500c9dcb9e15
iemwill <willie@laubenheimer.eu>
2020-05-06 14:30:39 +0200

    Projektdateien hinzufügen.

---
+++ Data/OptionData.cs
@@ -0,0 +1,16 @@
+﻿using ProtoBuf;
+using System.Data;
+
+
+namespace MasterComputations.Data
+{
+        [ProtoContract]
+        public class OptionData
+        {
+            [ProtoMember(1)]
+            public bool? isCall { get; set; }
+            [ProtoMember(2)]
+            public DataTable rawData { get; set; }
+            public OptionData() { isCall = null; rawData = new DataTable(); }
+        }
+}
---
+++ Data/Parse.cs
@@ -0,0 +1,42 @@
+﻿using System.Data;
+using System.IO;
+
+namespace MasterComputations.Data
+{
+    public class Parse
+    {
+        public static DataTable csv(string path, char seperator)
+        {
+            DataTable dt = new DataTable();
+
+            using (var fs = File.OpenRead(path))
+            {
+                using (var reader = new StreamReader(fs))
+                {
+                    string readHeader = reader.ReadLine();
+                    if (readHeader != null)
+                    {
+                        string[] header = readHeader.Split(seperator);
+                        int nSignals = header.GetLength(0);
+
+                        for (int j = 0; j < nSignals; j++)
+                            dt.Columns.Add(header[j], typeof(string));
+
+                        while (!reader.EndOfStream)
+                        {
+                            string sRead = reader.ReadLine();
+                            string[] splitArray = sRead.Split(seperator);
+                            var row = new object[nSignals];
+
+                            for (int j = 0; j < nSignals; j++)
+                                row[j] = splitArray[j];
+
+                            dt.Rows.Add(row);
+                        }
+                    }
+                }
+            }
+            return dt;
+        }
+    }
+}
