64b6606e008c17f4b8b9428ffab7f345568fe6ce
iemwill <willie@laubenheimer.eu>
2020-11-27 01:41:17 +0100

    g

---
+++ Computations/FirstLayer.cs
@@ -0,0 +1,24 @@
+﻿
+
+using MasterComputations.Data;
+using Org.OpenAPITools.Model;
+using System.Collections.Generic;
+
+namespace MasterComputations.Computations
+{
+    public class FirstLayer
+    {
+        public void trinominialTree(List<Trade> trades)
+        {
+
+        }
+        public void finiteDifference()
+        {
+
+        }
+        public void monteCarloSimulation(int simulationSize)
+        {
+
+        }
+    }
+}
